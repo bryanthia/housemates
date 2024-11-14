542c7bf440d8c004e23e88f84bcb96070da22229
bhthia1 <80899794+bhthia1@users.noreply.github.com>
2024-11-14 23:15:26 +1100

    first

---
+++ housemates-startup/housemates_startupApp.swift
@@ -0,0 +1,17 @@
+//
+//  housemates_startupApp.swift
+//  housemates-startup
+//
+//  Created by Bryan Thia on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct housemates_startupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
